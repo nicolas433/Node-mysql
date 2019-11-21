7e0c85ebf1e40cdd1e662b7c0dcd4c63830cea1b
nicolas433 <ngrisoste@gmail.com>
2019-11-20 21:17:27 -0300

    Criação da migrate e model de Addresses

---
+++ src/models/Address.js
@@ -0,0 +1,15 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Address extends Model {
+    static init(sequelize){
+        super.init({
+            zipcode: DataTypes.STRING,
+            street: DataTypes.STRING,
+            number: DataTypes.INTEGER,
+        },{
+            sequelize
+        })
+    }
+}
+
+module.exports = Address;
