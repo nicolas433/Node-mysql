f31c87d94faebbed47413675a717e76728078459
nicolas433 <ngrisoste@gmail.com>
2019-11-24 22:23:12 -0300

    Criação da função de delete para tech e tech_user

--- src/controllers/TechController.js
+++ src/controllers/TechController.js
@@ -23,5 +23,24 @@ module.exports = {
 
     async index(req, res) {
 
+    },
+    
+    async delete(req, res) {
+        const { user_id } = req.params;
+        const { name } = req.body;
+        
+        const user = await User.findByPk(user_id);
+
+        if(!user){
+            return res.status(400).json({ error: 'User not found' });
+        }
+        
+        const tech = await Tech.findOrCreate({
+            where: { name }
+        })
+
+        await user.removeTech(tech);
+
+        return res.json();
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ routes.post('/users/:user_id/addresses', AddressController.store);
 
 routes.get('/users/:user_id/techs', TechController.index);
 routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
 
 
 module.exports = routes;
