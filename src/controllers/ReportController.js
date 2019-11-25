bf67109cf91fec2c7486aaabf31d6b36c798d061
nicolas433 <ngrisoste@gmail.com>
2019-11-24 23:02:14 -0300

    Query complexa de exemplo (commit final)

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -1,7 +1,31 @@
+const { Op } = require('sequelize');
 const User = require('../models/User');
 
 module.exports = {
     async show(req, res) {
-        return res.json({ report: true })
+        const users = await User.findAll({
+            attributes: ['name', 'email'],
+            where: {
+                email: {
+                    [Op.like]: '%@gmail.com',
+                }
+            },
+            include: [
+                {association: 'addresses', 
+                    where: { 
+                        street: 'lugar nenhum' 
+                    } 
+                },
+                {association: 'techs', 
+                 required: false,
+                    where: { 
+                        name: {
+                            [Op.like]: 'react%',
+                        }
+                    } 
+                },
+            ]
+        })
+        return res.json(users);
     }
 }
